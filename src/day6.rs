3c8fcd09ae0c46e10be27cb70518e0256abc0b18
Edgar Onghena <dev@edgar.bzh>
2021-12-08 13:21:53 +0100

    Day 6 -- Part 2 (optimized)

--- src/day6.rs
+++ src/day6.rs
@@ -22,13 +22,21 @@ pub fn tick(lanterns: &mut Vec<u8>) {
 }
 
 fn simulate_for(input: &Vec<u8>, days: usize) -> usize {
-    std::iter::repeat(())
-        .take(days)
-        .fold(input.clone(), |mut lanterns, _| {
-            tick(&mut lanterns);
-            lanterns
-        })
-        .len()
+    let mut lanterns = [0usize; 9];
+
+    for day in input {
+        lanterns[*day as usize] += 1;
+    }
+
+    for _ in 0..days {
+        let l = lanterns[7];
+        lanterns[7] = lanterns[8];
+        lanterns[8] = lanterns[0];
+        lanterns[0..7].rotate_left(1);
+        lanterns[6] += l;
+    }
+
+    lanterns.into_iter().sum()
 }
 
 pub fn part_1(input: &Vec<u8>) -> usize {
